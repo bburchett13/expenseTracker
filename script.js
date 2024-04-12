5d0b3a59136fa59d648c1d497f07582b7a2a9147
Blake <bburchett13@gmail.com>
2024-04-12 15:59:46 -0400

    added function to display expense items after submission

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ let expenseDate = document.querySelector('#expenseDate');
 let expenseCategory = document.querySelector('#expenseCategory');
 let expenseValue = document.querySelector('#expenseValue');
 let expenseDescription = document.querySelector('#expenseDescription');
+let expenseList = document.querySelector('#expenseList');
 
 let submit = document.querySelector("#submit");
 submit.addEventListener('click', () => {
     newExpense = new expense(expenseName.value, expenseDate.value, expenseCategory.value, expenseValue.value, expenseDescription.value);
     expenses.push(newExpense);
+    showExpenses(newExpense);
 
 
 });
@@ -21,5 +23,50 @@ function expense(name, date, category, value, description) {
     this.category = category;
     this.value = value;
     this.description = description;
+
+}
+let nodeName;
+let nodeDate;
+let nodeCategory;
+let nodeValue;
+let nodeDescription;
+function showExpenses(expense) {
+
+    addingExpense = document.createElement('li');
+
+    nodeName = document.createElement('div');
+    nodeName.className = 'listItem';
+    nodeNameContent = document.createTextNode(expense.name);
+    nodeName.appendChild(nodeNameContent);
+
+    nodeDate = document.createElement('div');
+    nodeDate.className = 'listItem';
+    nodeDateContent = document.createTextNode(expense.date)
+    nodeDate.appendChild(nodeDateContent);
+
+    nodeCategory = document.createElement('div');
+    nodeCategory.className = 'listItem';
+    nodeCategoryContent = document.createTextNode(expense.category)
+    nodeCategory.appendChild(nodeCategoryContent);
+
+    nodeValue = document.createElement('div');
+    nodeValue.className = 'listItem';
+    nodeValueContent = document.createTextNode(expense.value)
+    nodeValue.appendChild(nodeValueContent);
+
+    nodeDescription = document.createElement('div');
+    nodeDescription.className = 'listItem';
+    nodeDescriptionContent = document.createTextNode(expense.description)
+    nodeDescription.appendChild(nodeDescriptionContent);
+
+
+    addingExpense.appendChild(nodeName);
+    addingExpense.appendChild(nodeDate);
+    addingExpense.appendChild(nodeCategory);
+    addingExpense.appendChild(nodeValue);
+    addingExpense.appendChild(nodeDescription);
+
+    expenseList.appendChild(addingExpense);
+    
 
 }
